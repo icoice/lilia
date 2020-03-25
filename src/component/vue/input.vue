136243d0e8e382e9a3c51465ca65fbdfb2160aa4
icoice <oncwnuDpAU1fGsBrJ3LcznYLBMiA@git.weixin.qq.com>
2020-03-25 18:03:58 +0800

    update

--- src/component/vue/input.vue
+++ src/component/vue/input.vue
@@ -2,8 +2,7 @@
 div.lilia-input.lilia
   div.input-mask(v-if='status === "disabled"')
   div.input-content
-    input(
-      ref='input'
+    input(ref='input'
       :value='val === null ? "" : val'
       :type='type'
       :maxLength='max'
@@ -15,7 +14,7 @@ div.lilia-input.lilia
       @keyup='e => eventAction("keyup", e)'
       @keydown='e => eventAction("keydown", e)')
     lilia-button(@pressEnd='e => eventAction("clear", e)')
-      span.iconfont.icon-clear(slot='button'
+      span.lilia-common-icon.lilia-common-close(slot='button'
         v-if='val !== null && val !== "" && status !== "disabled"')
       span(slot='button' v-else)
 </template>
