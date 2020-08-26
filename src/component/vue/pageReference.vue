c3738525d73c3190739f3df82da6421875d98361
root <985611106@qq.com>
2020-08-26 17:48:29 +0800

    update

--- src/component/vue/pageReference.vue
+++ src/component/vue/pageReference.vue
@@ -4,17 +4,17 @@
       tr
         td.page-operate
           lilia-button(@pressEnd='firstPageNo')
-            span(slot='button') first
+            span.fa.fa-fast-backward(slot='button')
           lilia-button(@pressEnd='e => tapPageNo(currentNo - 1)')
-            span(slot='button') prev
+            span.fa.fa-step-backward(slot='button')
         td.page-no.animated.fadeIn(v-for='pageNo in pageNoList' v-if='!maxPage(pageNo)')
           lilia-button(@pressEnd='e => tapPageNo(pageNo)')
             span(slot='button' :class='selected(pageNo)') {{ format(pageNo) }}
         td.page-operate
           lilia-button(@pressEnd='e => tapPageNo(currentNo + 1)')
-            span(slot='button') next
+            span.fa.fa-step-forward(slot='button')
           lilia-button(@pressEnd='e => tapPageNo(getTotalPage())')
-            span(slot='button') last
+            span.fa.fa-fast-forward(slot='button')
         td.page-no-input
           lilia-input(placeholder='页码' :value='String(currentNo)' :filter='/[^0-9]/' @keyup='inputPageNo')
         td.page-info
